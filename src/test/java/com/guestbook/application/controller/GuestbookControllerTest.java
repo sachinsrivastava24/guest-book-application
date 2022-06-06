5abb3affc566e061939e65cdf261e7c43b6fb70a
Sachin Srivastava <sonu.srivastava24@gmail.com>
2022-06-06 23:49:40 +0100

    Added test classes

---
+++ src/test/java/com/guestbook/application/controller/GuestbookControllerTest.java
@@ -0,0 +1,69 @@
+package com.guestbook.application.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import com.guestbook.application.entity.GuestBookEntity;
+import com.guestbook.application.repository.GuestBookRepository;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+/**
+ * Integration tests for {@link GuestBookController}.
+ *
+ * @author Oliver Drotbohm
+ */
+@SpringBootTest
+@AutoConfigureMockMvc
+public class GuestbookControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Autowired
+    GuestBookRepository repository;
+
+    @Test
+    void testGetEndPointsForGuestEntryById() throws Exception {
+        GuestBookEntity entry = repository.findAll().iterator().next();
+        mvc.perform(get("/guestEntryById/{id}", entry.getId()))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testGetEndPointsForGuestEntryByName() throws Exception {
+        GuestBookEntity entry = repository.findAll().iterator().next();
+        mvc.perform(get("/guestEntryByName/{name}", entry.getName()))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testPostEndPointsForAddGuestBookEntry() throws Exception {
+        GuestBookEntity entry = repository.findAll().iterator().next();
+        mvc.perform(MockMvcRequestBuilders
+                .post("/addGuestBookEntry"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void testPutEndPointsForUpdateGuestBookEntry() throws Exception {
+        GuestBookEntity entry = repository.findAll().iterator().next();
+        entry.setId(1);
+        mvc.perform(MockMvcRequestBuilders
+                        .put("/updateGuestBookEntry"))
+                .andExpect(status().isBadRequest());
+        Assert.assertEquals(1, entry.getId());
+    }
+
+    @Test
+    void testDeleteEndPointsForDeleteGuestEntry() throws Exception {
+        GuestBookEntity entry = repository.findAll().iterator().next();
+        mvc.perform(MockMvcRequestBuilders
+                        .delete("/deleteGuestEntry/{id}", entry.getId()))
+                .andExpect(status().isOk());
+    }
+}
---
+++ src/test/java/com/guestbook/application/repository/GuestbookRepositoryTest.java
@@ -0,0 +1,37 @@
+package com.guestbook.application.repository;
+
+import com.guestbook.application.entity.GuestBookEntity;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.*;
+import javax.transaction.Transactional;
+
+/**
+ * Integration tests for {@link GuestBookRepository}.
+ * <p>
+ * Bootstraps the application using the {@link com.guestbook.application.GuestBookApplication} configuration class. Enables transaction rollbacks after
+ * test methods using the {@link Transactional} annotation.
+ *
+ * @author Oliver Drotbohm
+ * @author Paul Henke
+ */
+@SpringBootTest
+@Transactional
+public class GuestbookRepositoryTest{
+
+	@Autowired
+	GuestBookRepository repository;
+
+	@Test
+	void persistsGuestbookEntry() {
+		GuestBookEntity entry = repository.save(new GuestBookEntity(1,"Sachin", "I am a guest"));
+		assertThat(repository.findAll()).contains(entry);
+	}
+
+	@Test
+	void findsGuestbookEntryByName() {
+		GuestBookEntity entry = repository.save(new GuestBookEntity(3,"Srivastava", "I am also a guest"));
+		assertThat(repository.findByName("Srivastava")).doesNotHaveToString("Sachin");
+	}
+}
